de3fcebfd035d3f01374b0d3d4f69f09c5c192a6
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-02-11 18:55:38 +0000

    tests edit

--- cypress/e2e/homeButton.cy.js
+++ cypress/e2e/homeButton.cy.js
@@ -2,6 +2,6 @@ describe("Home page", () => {
   it("User sees hello button", () => {
     cy.visit("/");
 
-    cy.contains("hedllo").should("be.visible");
+    cy.contains("hello").should("be.visible");
   });
 });
